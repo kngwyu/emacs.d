9a7ca046025ecbfb5e8de9583127e112a7adc743
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-06-24 17:19:59 +0900

    Use CamingoCode only when it is available

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -2,7 +2,8 @@
 ;;; Commentary:
 ;;; Code:
 
-(set-face-attribute 'default nil :family "Camingo Code" :height 124)
+(when (member "CamingoCode" (font-family-list))
+  (set-face-attribute 'default nil :family "Camingo Code" :height 124)  )
 (set-fontset-font t 'japanese-jisx0208 (font-spec :family "IPA Gothic" :height 124))
 (maybe-require-package 'dracula-theme)
 
