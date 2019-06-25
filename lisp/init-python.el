c7c0097d4169424740e4b021739681556489dedd
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-06-25 12:15:04 +0900

    Use Python3 for jedi

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -12,6 +12,8 @@
 (when (maybe-require-package 'jedi-core)
   (setq-default jedi:complete-on-dot t)
   (setq-default jedi:use-shortcuts t)
+  (setq jedi:environment-virtualenv
+        (list "virtualenv" "--python" "python3" "--system-site-packages"))
   (after-load 'python
     (add-hook 'python-mode-hook 'jedi:setup)))
 
