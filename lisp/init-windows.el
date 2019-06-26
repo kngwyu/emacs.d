ea4e0f3290c76ef1a5c7071210e8a45acc133b6c
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-06-26 19:23:45 +0900

    Use customized C-x 1/2/3

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -24,28 +24,28 @@
 ;;----------------------------------------------------------------------------
 ;; When splitting window, show (other-buffer) in the new window
 ;;----------------------------------------------------------------------------
-;; (defun split-window-func-with-other-buffer (split-function)
-;;   (lambda (&optional arg)
-;;     "Split this window and switch to the new window unless ARG is provided."
-;;     (interactive "P")
-;;     (funcall split-function)
-;;     (let ((target-window (next-window)))
-;;       (set-window-buffer target-window (other-buffer))
-;;       (unless arg
-;;         (select-window target-window)))))
+(defun split-window-func-with-other-buffer (split-function)
+  (lambda (&optional arg)
+    "Split this window and switch to the new window unless ARG is provided."
+    (interactive "P")
+    (funcall split-function)
+    (let ((target-window (next-window)))
+      (set-window-buffer target-window (other-buffer))
+      (unless arg
+        (select-window target-window)))))
 
-;; (global-set-key (kbd "C-x 2") (split-window-func-with-other-buffer 'split-window-vertically))
-;; (global-set-key (kbd "C-x 3") (split-window-func-with-other-buffer 'split-window-horizontally))
+(global-set-key (kbd "C-x 2") (split-window-func-with-other-buffer 'split-window-vertically))
+(global-set-key (kbd "C-x 3") (split-window-func-with-other-buffer 'split-window-horizontally))
 
-;; (defun sanityinc/toggle-delete-other-windows ()
-;;   "Delete other windows in frame if any, or restore previous window config."
-;;   (interactive)
-;;   (if (and winner-mode
-;;            (equal (selected-window) (next-window)))
-;;       (winner-undo)
-;;     (delete-other-windows)))
+(defun sanityinc/toggle-delete-other-windows ()
+  "Delete other windows in frame if any, or restore previous window config."
+  (interactive)
+  (if (and winner-mode
+           (equal (selected-window) (next-window)))
+      (winner-undo)
+    (delete-other-windows)))
 
-;; (global-set-key (kbd "C-x 1") 'sanityinc/toggle-delete-other-windows)
+(global-set-key (kbd "C-x 1") 'sanityinc/toggle-delete-other-windows)
 
 ;;----------------------------------------------------------------------------
 ;; Rearrange split windows
