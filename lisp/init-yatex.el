ab2834d30f1b3379527d9e6b9287683d3496b5c7
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-07-05 02:08:35 +0900

    Fix init-yatex

--- lisp/init-yatex.el
+++ lisp/init-yatex.el
@@ -6,11 +6,12 @@
 ;;; YaTeX configuration option
 (when (maybe-require-package 'yatex)
   (setq auto-mode-alist (cons '("\\.tex$" . yatex-mode) auto-mode-alist))
+  (autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
   (setq-default YaTeX-latex-message-code 'utf-8)
   (setq-default YaTeX-use-LaTeX2e t)
-  ;; (setq tex-command "latexmk -lualatex")
-  ;; (setq tex-command "latexmk -gg -pdf")
-  (setq-default tex-command "latexmk -pdf -shell-escape")
+  (setq-default tex-command "latexmk -lualatex")
+  ;; (setq-default tex-command "latexmk -gg -pdf")
+  ;; (setq-default tex-command "latexmk -pdf -shell-escape")
   (setq-default dvi2-command "evince"))
 
 (provide 'init-yatex)
