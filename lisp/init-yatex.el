8dee19978f8d79717967a9b7eafe25f16877ca01
kngwyu <yuji.kngw.80s.revive@gmail.com>
2020-01-19 00:10:12 +0900

    Use -gg -pdf in yatex setting
    Change $pdflatex = ...; in .latexmkrc

--- lisp/init-yatex.el
+++ lisp/init-yatex.el
@@ -9,9 +9,7 @@
   (autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
   (setq-default YaTeX-latex-message-code 'utf-8)
   (setq-default YaTeX-use-LaTeX2e t)
-  (setq-default tex-command "latexmk -lualatex")
-  ;; (setq-default tex-command "latexmk -gg -pdf")
-  ;; (setq-default tex-command "latexmk -pdf -shell-escape")
+  (setq-default tex-command "latexmk -gg -pdf")
   (setq-default dvi2-command "evince"))
 
 (provide 'init-yatex)
