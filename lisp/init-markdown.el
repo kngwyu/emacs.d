ba4a7b2e7c196f954e86b716ac2d3fb6314937ce
kngwyu <yuji.kngw.80s.revive@gmail.com>
2020-10-05 13:15:35 +0900

    Use indicator with md

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -4,9 +4,10 @@
 
 (when (maybe-require-package 'markdown-mode)
   (add-auto-mode 'markdown-mode "\\.md\\.html\\'")
+  (when (boundp 'display-fill-column-indicator)
+    (add-hook 'mark-mode-hook 'display-fill-column-indicator-mode))
   (with-eval-after-load 'whitespace-cleanup-mode
     (add-to-list 'whitespace-cleanup-mode-ignore-modes 'markdown-mode)))
-
 
 (provide 'init-markdown)
 ;;; init-markdown.el ends here
